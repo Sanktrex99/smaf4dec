b4ead15e8f94915cc7803227a08786c1c886bbdb
arianna choza <unachoza@gmail.com>
2020-06-14 11:26:40 -0400

    fixed getdata hook

--- decho/src/Context/UserProvider.context.js
+++ decho/src/Context/UserProvider.context.js
@@ -1,15 +1,19 @@
 import React, { createContext, useState, useEffect } from 'react';
-const context = createContext(null);
 import axios from 'axios';
+
+const context = createContext(null);
 
 const UserProvider = ({ children }) => {
   const [user, setUser] = useState({});
 
-  useEffect(async () => {
-    const response = await axios.get('/user');
-    setUser(response).catch((err) => {
-      console.log(err, 'maybe just data and no errors', user);
-    });
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get('/user');
+      setUser(response).catch((err) => {
+        console.log(err, 'maybe just data and no errors', user);
+      });
+    };
+    fetchData();
   }, []);
 
   return <context.Provider value={user}>{children}</context.Provider>;
